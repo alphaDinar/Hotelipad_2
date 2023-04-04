216f8d4d96e90faa6edba1debec8c911a85cdeaf
alphaDinar <yanksone18@yahoo.com>
2023-04-04 10:19:32 +0000

    change

---
+++ js/swiper.js
@@ -0,0 +1,26 @@
+window.onload =()=>{
+  makeHostelSwiper()
+}
+window.onresize =()=>{
+  makeHostelSwiper()
+}
+
+const makeHostelSwiper =()=>{
+  if(window.innerWidth < 840 && window.innerWidth >  450){var slideNum = 2}else if(window.innerWidth  < 450){var slideNum = 1}else{var slideNum = 3}
+    var hostel_swiper = new Swiper(".hostel_swiper", {
+      slidesPerView: slideNum,
+      spaceBetween: 20,
+      loop:true,
+    speed: 1000,
+    autoplay:{
+      delay:3500
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  hostel_swiper.el.onmouseenter =()=>{
+    hostel_swiper.autoplay.stop()
+  }
+}   
